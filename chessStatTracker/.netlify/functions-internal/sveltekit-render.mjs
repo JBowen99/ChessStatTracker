f2cfb0adb3d5be85a05ffe9c4de0f15f8a8f43fa
JBowen99 <josephwbowen99@gmail.com>
2024-09-11 19:12:36 -0600

    updated netlify build

--- chessStatTracker/.netlify/functions-internal/sveltekit-render.mjs
+++ chessStatTracker/.netlify/functions-internal/sveltekit-render.mjs
@@ -12,7 +12,7 @@ return {
 	assets: new Set(["favicon.png","logo.svg","faviconRook.png","logo3.svg"]),
 	mimeTypes: {".png":"image/png",".svg":"image/svg+xml"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.C1zWonoJ.js","app":"_app/immutable/entry/app.COsRdVNu.js","imports":["_app/immutable/entry/start.C1zWonoJ.js","_app/immutable/chunks/entry.DtcCO3xn.js","_app/immutable/chunks/scheduler.3q3s62UL.js","_app/immutable/chunks/index.BIZQkIVS.js","_app/immutable/entry/app.COsRdVNu.js","_app/immutable/chunks/scheduler.3q3s62UL.js","_app/immutable/chunks/index.CfPbG8to.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.C2BGB4Ef.js","app":"_app/immutable/entry/app.CIhuH3ib.js","imports":["_app/immutable/entry/start.C2BGB4Ef.js","_app/immutable/chunks/entry.DODRD0AY.js","_app/immutable/chunks/scheduler.3q3s62UL.js","_app/immutable/chunks/index.BIZQkIVS.js","_app/immutable/entry/app.CIhuH3ib.js","_app/immutable/chunks/scheduler.3q3s62UL.js","_app/immutable/chunks/index.CfPbG8to.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('../server/nodes/0.js')),
 			__memo(() => import('../server/nodes/1.js')),
